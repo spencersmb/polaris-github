a96608814fc30ca0edd601ee7a1e9f663db0f3d9
spencersmb <spencer.bigum@gmail.com>
2015-06-26 14:18:41 -0400

    desktop /tablet nav working

---
+++ assets/scripts/main.js
@@ -0,0 +1,5 @@
+  jQuery(document).ready(function($){
+	$( '#dl-menu' ).dlmenu({
+	             animationClasses : { classin : 'animation-class-name', classout : 'animation-class-name' }
+	         });
+  });
