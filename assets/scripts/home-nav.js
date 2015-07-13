819d242bd27bc6697e1b724e548e4bb42058a84f
Casey Jardin <casey@iconologic.com>
2015-07-13 12:59:04 -0400

    Update home + global headline padding

--- assets/scripts/home-nav.js
+++ assets/scripts/home-nav.js
@@ -1,4 +1,5 @@
 $(function(){
+
     
     $("label.btn").on('click',function () {
     	var choice = $(this).find('input:radio').val();
@@ -6,14 +7,29 @@ $(function(){
         // What am I clicking?
         console.log(choice);
 
+        // MATCH HEIGHT
+        // getHeight = $(".quiz").height();
+        // console.log(getHeight);
+        // getHeight;
+
+        // setHeight = $("#psn-nav-home").css("height", $(".quiz").height()+387);
+        // // getHeight = $('.quiz').height();
+        // // findContainer = $('#psn-nav-home').height();
+
+        // // console.log(getHeight);
+        // // console.log(findContainer);
+        // console.log(setHeight);
+
         // Go back to Home
         if (choice == 0) {
+            getHeight;
             $('#nav_why').fadeOut();
             $('#nav_0').fadeIn();
         }
 
         // Why Polaris
         if (choice == 1) {
+            getHeight;
             $('#nav_0').fadeOut();
             $('#nav_why').fadeIn();
         }
@@ -24,4 +40,5 @@ $(function(){
         }
     });
 
+    
 });	
