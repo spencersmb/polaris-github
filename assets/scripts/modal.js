a8b632215b54577f0458c03ce5109b7f5f5e8c4e
Casey Jardin <casey@iconologic.com>
2015-07-27 16:24:30 -0400

    Added 2 modal pages for new visitors

---
+++ assets/scripts/modal.js
@@ -0,0 +1,11 @@
+$(function () {
+	// Disable Scrolling
+	$('html, body').css({
+	    'overflow': 'hidden',
+	    'height': '100%'
+	});
+
+	$(window).load(function(){
+	    $('#largeModal').modal('show');
+	});
+});
